8630acf02b5985d173e47e6ed140e7a10278fa9d
Prince Kumar <kumarprincnxncbe89093@gmail.com>
2026-06-13 20:38:49 +0530

    Update project

--- pkg_resources.py
+++ pkg_resources.py
@@ -1,5 +1,23 @@
 from importlib import import_module
+from importlib.metadata import PackageNotFoundError, version
 from pathlib import Path
+
+
+class Distribution:
+    def __init__(self, package_name):
+        self.project_name = package_name
+        self.version = version(package_name)
+
+
+class DistributionNotFound(Exception):
+    pass
+
+
+def get_distribution(package_name):
+    try:
+        return Distribution(str(package_name).split()[0])
+    except PackageNotFoundError as exc:
+        raise DistributionNotFound(package_name) from exc
 
 
 def resource_filename(package_or_requirement, resource_name):
